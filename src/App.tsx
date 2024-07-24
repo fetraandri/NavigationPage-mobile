9d2f9610b440ebbb71af60984d976c87ff153d73
fetraandri <hei.fetra@gmail.com>
2024-07-24 03:57:13 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-/**
+    /**
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
@@ -76,7 +76,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
